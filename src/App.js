606e9662e770bdd2def2312c704d82afd77f7112
Joao Oliveira <vitinho.oliv@gmail.com>
2021-09-27 13:01:30 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
+function App(){
+
+  const name = "DarkSide Dev";
+  const [idade, setIdade] = useState(18);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [show, setShow] = useState(false);
+
+  const calc = () => {
+    setShow(true);
+  };
+
+  return(
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2>Hey, {name} here</h2>
+
+      <p>I'm {idade} years old</p>
+      <p>Let's code</p>
+
+      <br/>
+
+      <input placeholder="Digite um número" onChange={e => {setNumber1(e.target.value);setShow(false);}} type="number" value={number1}/>
+      <input placeholder="Digite outro número" onChange={e => {setNumber2(e.target.value);setShow(false);}} type="number" value={number2}/>
+      <br/>
+      <button onClick={calc}>Calculate</button>
+
+      <br/>
+
+      { show && (number1 != 0 && number2 != 0) &&
+
+        <>
+          <p>{number1} + {number2} = {parseFloat(number1)+parseFloat(number2)}</p>
+          <p>{number1} - {number2} = {number1-number2}</p>
+          <p>{number1} * {number2} = {number1*number2}</p>
+          <p>{number1} / {number2} = {(number1/number2).toFixed(2)}</p>
+        </>
+
+      }
+
+
+
     </div>
+
   );
+
 }
 
 export default App;
